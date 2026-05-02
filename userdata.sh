1c87aa8ff1d63807504a16e74fd9eaa2ff92a970
AydinTokuslu <aydintokuslu78@gmail.com>
2026-05-02 16:45:18 +0200

    updates3

--- userdata.sh
+++ userdata.sh
@@ -5,7 +5,6 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install git -y
 apt-get install -y python3 python3-pip python3-venv python3-dev
-
 python3 -m venv /opt/appenv
 source /opt/appenv/bin/activate
 pip install --upgrade pip
